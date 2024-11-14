d8d338c15a14bd081290bb8190f75cc61263ae88
joe <joe.hasson2@gmail.com>
2024-11-14 13:01:10 +0000

    Specify user and host properly in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "$PRIVATE_KEY" > private_key.pem
 chmod 600 private_key.pem
 
 # Deploy using SSH with fresh clone
-ssh -o StrictHostKeyChecking=no -i private_key.pem "$USER@$HOST" '\
+ssh -o StrictHostKeyChecking=no -i private_key.pem "ec2-user@$EC2_IP" '\
   git clone https://github.com/${REPO}.git /var/www/fastapi &&\
   cd /var/www/fastapi &&\
   source venv/bin/activate &&\
