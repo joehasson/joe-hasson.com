c728891776c012113b1de5bbd23e94079430ddd4
joe <joe.hasson2@gmail.com>
2024-11-14 21:31:19 +0000

    Add CSS bundling and SSR (inlining)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,6 @@ ssh -o StrictHostKeyChecking=no -i private_key.pem "ec2-user@$EC2_IP" "\
   sudo rm -rf /var/www/fastapi &&\
   sudo git clone https://github.com/${REPO}.git /var/www/fastapi &&\
   cd /var/www/fastapi &&\
-  pip3 install fastapi uvicorn gunicorn jinja2 &&\
+  pip3 install fastapi uvicorn gunicorn jinja2 webassets cssmin &&\
   sudo systemctl restart fastapi
 "
--- src/app.py
+++ src/app.py
@@ -2,26 +2,52 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import Response
+from fastapi.middleware.gzip import GZipMiddleware
+from webassets import Environment, Bundle
+
 
 app = FastAPI()
+app.add_middleware(GZipMiddleware)
 templates = Jinja2Templates(directory="templates")
+
+# Mounts for static content
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/webfonts", StaticFiles(directory="static/webfonts"), name="static")
+
+assets = Environment()
+assets = Environment(directory='static', url='/static')
+bundle_output = 'gen/packed.css'
+css = Bundle(
+    'base.css',
+    'blog.css',
+    'cv.css',
+    'navbar.css',
+    'portfolio.css',
+    'fontawesome/all.min.css',
+    filters='cssmin',
+    output=bundle_output
+)
+assets.register('css_all', css)
+css.build()
+
+with open(f'static/{bundle_output}') as f:
+    BUNDLED_CSS = f.read()
 
 @app.get("/")
 async def home(request: Request):
-    return templates.TemplateResponse("home.html", dict(request=request))
+    return templates.TemplateResponse("home.html", dict(request=request, css=BUNDLED_CSS))
 
 @app.get("/blog")
 async def blog(request: Request):
-    return templates.TemplateResponse("blog.html", dict(request=request))
+    return templates.TemplateResponse("blog.html", dict(request=request, css=BUNDLED_CSS))
 
 @app.get("/portfolio")
 async def portfolio(request: Request):
-    return templates.TemplateResponse("portfolio.html", dict(request=request))
+    return templates.TemplateResponse("portfolio.html", dict(request=request, css=BUNDLED_CSS))
 
 @app.get("/cv")
 async def cv(request: Request):
-    return templates.TemplateResponse("cv.html", dict(request=request))
+    return templates.TemplateResponse("cv.html", dict(request=request, css=BUNDLED_CSS))
 
 @app.get("/health_check")
 async def health_check():
