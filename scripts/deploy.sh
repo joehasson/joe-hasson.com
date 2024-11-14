53f1fcf2decaa23667d59023dbf987d66e1499f6
joe <joe.hasson2@gmail.com>
2024-11-14 15:14:45 +0000

    install jinja2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,6 @@ ssh -o StrictHostKeyChecking=no -i private_key.pem "ec2-user@$EC2_IP" "\
   sudo rm -rf /var/www/fastapi &&\
   sudo git clone https://github.com/${REPO}.git /var/www/fastapi &&\
   cd /var/www/fastapi &&\
-  pip3 install fastapi uvicorn gunicorn &&\
+  pip3 install fastapi uvicorn gunicorn jinja2 &&\
   sudo systemctl restart fastapi
 "
