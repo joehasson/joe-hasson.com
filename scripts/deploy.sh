a18c83264204cd63e9de67533d7b029af9765438
joe <joe.hasson2@gmail.com>
2024-11-14 12:54:02 +0000

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,9 +7,9 @@ echo "$PRIVATE_KEY" > private_key.pem
 chmod 600 private_key.pem
 
 # Deploy using SSH with fresh clone
-ssh -o StrictHostKeyChecking=no -i private_key.pem $USER@$HOST "
-  git clone https://github.com/${REPO}.git /var/www/fastapi
-  cd /var/www/fastapi
-  source venv/bin/activate
+ssh -o StrictHostKeyChecking=no -i private_key.pem "$USER@$HOST" '\
+  git clone https://github.com/${REPO}.git /var/www/fastapi &&\
+  cd /var/www/fastapi &&\
+  source venv/bin/activate &&\
   sudo systemctl restart fastapi
-"
+'
