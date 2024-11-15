90d9b7b7a2b22b101d94c0d7baeb54f179cbbfc7
joe <joe.hasson2@gmail.com>
2024-11-15 12:38:12 +0000

    Remove caching page rule
    Its deprecated and cloudflare does some caching by anway.
    Will add headers with nginx or fastapi to signal this.

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -7,20 +7,3 @@ resource "cloudflare_dns_record" "root" {
   type    = "A"
   proxied = true  # This enables Cloudflare's SSL and CDN
 }
-
-# Add cache rules
-resource "cloudflare_page_rule" "cache_everything" {
-  zone_id = var.cloudflare_zone_id
-  targets = [{
-    target = "url"
-    constraint = {
-      operator = "matches"
-      value = "joe-hasson.com/*"
-    }
-  }]
-  
-  actions = [{
-    cache_level = "cache_everything"
-    edge_cache_ttl = 1800  # Cache for ~30 mins
-  }]
-}
