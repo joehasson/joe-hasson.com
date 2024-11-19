571a5992b6c920f09e96cc303c0f80946869a046
joe <joe.hasson2@gmail.com>
2024-11-19 18:55:15 +0000

    Update script to run locally

--- dev_scripts/run_server.sh
+++ dev_scripts/run_server.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
 # Work in project root
-cd "$(dirname $0)/.."
+cd $(dirname $0)/..
 
-# Render static content
-python3 static_site/render.py
-
-# Run nginx
-docker run \
-    -v "$(pwd)/nginx.conf:/etc/nginx/nginx.conf" \
-    -v "$(pwd)/static_site/build:/static" \
-    -w '/' \
-    -p 8000:80 \
-    nginx nginx -g 'daemon off;'
+docker build . -t site
+docker run -p 8000:80 site
