40607346da46e23f949955f1eb75c219e53e6c49
joe <joe.hasson2@gmail.com>
2024-11-20 21:31:07 +0000

    Explicitly handle sigint in script

--- dev_scripts/quick_serve.py
+++ dev_scripts/quick_serve.py
@@ -8,27 +8,38 @@ from pathlib import Path
 import os
 import shutil
 import http.server
+import signal
+import sys
 
 import jinja2
 import webassets
 
 def remove_file(path):
+    # make sure we're working from root
+    print("Cleaning up", path)
     if os.path.isfile(path):
         os.remove(path)
 
 def remove_dir(dir):
-    shutil.rmtree(dir, ignore_errors=True)
+    # make sure we're working from root
+    print("Cleaning up", dir)
+    shutil.rmtree(dir, ignore_errors=False)
 
+def sigint_handler(_signum, _fname):
+    print("\nSIGINT Received. Exiting...", file=sys.stderr,)
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, sigint_handler)
 
 if __name__ == "__main__":
     # Work in static_site root
     os.chdir(Path(__file__).parent.parent)
     
     # Temp files
-    _TEMP_BUNDLE = "_bundle.css"
-    _TEMP_STATIC_DIR = "_static"
+    _TEMP_BUNDLE = Path("_bundle.css").resolve()
+    _TEMP_STATIC_DIR = Path("_static").resolve()
 
-    atexit.register(remove_file, f'styles/{_TEMP_BUNDLE}')
+    atexit.register(remove_file, _TEMP_BUNDLE)
     atexit.register(remove_dir, _TEMP_STATIC_DIR)
 
     # Perform CSS Bundling
@@ -41,12 +52,12 @@ if __name__ == "__main__":
         'navbar.css',
         'portfolio.css',
         filters='cssmin',
-        output=_TEMP_BUNDLE
+        output=str(_TEMP_BUNDLE)
     )
     assets.register('css_all', css)
     css.build()
     
-    with open(f'styles/{_TEMP_BUNDLE}') as f:
+    with open(_TEMP_BUNDLE) as f:
         BUNDLED_CSS = f.read()
     
     # SSR with jinja2 and leave generated static content in build directory
@@ -56,10 +67,9 @@ if __name__ == "__main__":
     os.makedirs(_TEMP_STATIC_DIR)
     for fname in ["index.html", "blog.html", "portfolio.html", "cv.html"]:
         # Strip html from filename as hack - extension not included in links
-        with open(f'{_TEMP_STATIC_DIR}/{fname.strip(".html")}', 'w') as f:
+        with open(_TEMP_STATIC_DIR / fname.strip(".html"), 'w') as f:
             f.write(render(fname))
 
-    # Serve static content
     os.chdir(_TEMP_STATIC_DIR)
     port = 8000
     # Default to html content type for endpoints - they don't include .html
@@ -68,5 +78,4 @@ if __name__ == "__main__":
     httpd = http.server.HTTPServer(('', port), http.server.SimpleHTTPRequestHandler)
     print("Serving on port", port)
     httpd.serve_forever()
-
     
