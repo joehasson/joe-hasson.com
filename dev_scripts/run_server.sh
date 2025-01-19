b63a502c02cbb1996212d0a849265292fba1efc1
joe <joe.hasson2@gmail.com>
2025-01-19 17:18:00 +0000

    Deploy containers to server via gh actions

--- dev_scripts/run_server.sh
+++ dev_scripts/run_server.sh
@@ -3,5 +3,6 @@
 # Work in project root
 cd $(dirname $0)/..
 
-docker build . -t site
+docker build . -t joe-hasson-personal-site
+docker build . -f Dockerfile.migrations -t joe-hasson-personal-site-migrations
 docker run -p 8000:80 site
