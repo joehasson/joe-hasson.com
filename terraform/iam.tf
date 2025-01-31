9ecc1d5b8565f5742221d7283d0180426ea111b8
joe <joe.hasson2@gmail.com>
2025-01-31 02:28:21 +0000

    Allow attach to ec2 via ssm

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -36,6 +36,11 @@ resource "aws_iam_role_policy" "ecr_policy" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2_ecr_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2-ecr-profile"
   role = aws_iam_role.ec2_ecr_role.name
